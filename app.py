0c7568fcd73e1ef330d2a78449ed59a84aacba7d
Karen Javadyan <kajarenc@gmail.com>
2022-11-20 02:02:31 +0400

    add app code

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
-st.write("Hello World!")
+
+with st.echo():
+    st.title("Static Demo test app")
+
+    raw_html = """ <img src="user-static/dog.jpg">"""
+
+    st.markdown(raw_html, unsafe_allow_html=True)
