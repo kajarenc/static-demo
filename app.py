e2d6448dcda1043222921a07b7bd80f7228fc89b
Karen Javadyan <kajarenc@gmail.com>
2022-11-20 18:42:45 +0400

    add pure markdown example

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Static demo app", page_icon=":hatching_chick:", layout="wide"
+)
 
-with st.echo():
-    st.title("Static Demo test app")
+left, right = st.columns(2)
 
-    raw_html = """ <img src="user-static/dog.jpg">"""
+with left:
+    with st.echo():
+        st.title("Static Demo test app")
+        raw_html = """<img src="user-static/dog.jpg"> </br></br></br>"""
+        st.markdown(raw_html, unsafe_allow_html=True)
 
-    st.markdown(raw_html, unsafe_allow_html=True)
+with right:
+    with st.echo():
+        st.title("Exclusive content, clickable image!")
+
+        st.markdown("[![Click me](user-static/cat.png)](https://youtu.be/dQw4w9WgXcQ)")
