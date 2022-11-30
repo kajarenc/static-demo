4c8ac501f84c0bd58d2067e386a0d45ae7e52fa8
Karen Javadyan <kajarenc@gmail.com>
2022-11-30 18:18:00 +0400

    use new wheel file, static folder should be not configurable, ./static/ ,  url host/app/static/filename.jpeg

--- app.py
+++ app.py
@@ -4,16 +4,21 @@ st.set_page_config(
     page_title="Static demo app", page_icon=":hatching_chick:", layout="wide"
 )
 
+st.write("HELLO WORLD")
+
+x = st.slider("AAAA", 0, 100, 50)
+st.write(x**2)
+
 left, right = st.columns(2)
 
 with left:
     with st.echo():
         st.title("Static Demo test app")
-        raw_html = """<img src="user-static/dog.jpg"> </br></br></br>"""
+        raw_html = """<img src="app/static/dog.jpg"> </br></br></br>"""
         st.markdown(raw_html, unsafe_allow_html=True)
 
 with right:
     with st.echo():
         st.title("Exclusive content, clickable image!")
 
-        st.markdown("[![Click me](user-static/cat.png)](https://youtu.be/dQw4w9WgXcQ)")
+        st.markdown("[![Click me](app/static/cat.png)](https://youtu.be/dQw4w9WgXcQ)")
---
+++ pages/page1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.write("PAGE!")
+
+
+st.download_button("AAAA", "BBBBB")
