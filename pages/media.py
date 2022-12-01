f3f006bdbfd0bcd8170e1bac068ff06b82448149
Karen Javadyan <kajarenc@gmail.com>
2022-12-01 18:54:15 +0400

    add img width

--- pages/media.py
+++ pages/media.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.write("ANOTHER PAGE")
 
-raw_html = """<img src="./app/static/Parrot.jpg"> </br></br></br>"""
+raw_html = """<img src="./app/static/Parrot.jpeg" width="800"> </br></br></br>"""
 st.markdown(raw_html, unsafe_allow_html=True)
--- pages/page1.py
+++ pages/page1.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("PAGE 1")
 
 
-raw_html = """<img src="./app/static/Parrot.jpeg"> </br></br></br>"""
+raw_html = """<img src="./app/static/Parrot.jpeg" width="500"> </br></br></br>"""
 st.markdown(raw_html, unsafe_allow_html=True)
