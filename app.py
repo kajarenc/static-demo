c06bffee1b8155052ea852690030523a37497f28
Karen Javadyan <kajarenc@gmail.com>
2022-12-01 19:01:04 +0400

    change titles

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.set_page_config(
     page_title="Static demo app", page_icon=":hatching_chick:", layout="wide"
 )
 
-st.write("HELLO WORLD")
+st.title("Static demo app")
 
 x = st.slider("AAAA", 0, 100, 50)
 st.write(x**2)
@@ -13,12 +13,11 @@ left, right = st.columns(2)
 
 with left:
     with st.echo():
-        st.title("Static Demo test app")
-        raw_html = """<img src="./app/static/dog.jpg"> </br></br></br>"""
+        st.title("DOG")
+        raw_html = """<img src="./app/static/dog.jpg"> </br>"""
         st.markdown(raw_html, unsafe_allow_html=True)
 
 with right:
     with st.echo():
-        st.title("Exclusive content, clickable image!")
-
+        st.title("CAT")
         st.markdown("[![Click me](app/static/cat.png)](https://youtu.be/dQw4w9WgXcQ)")
